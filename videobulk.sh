7c3fc91b96dfc68b5c597f48f79dc10ab36a4eb1
rafa <rafael9876>
2016-04-26 13:55:54 +0200

    videobulk

--- videobulk.sh
+++ videobulk.sh
@@ -7,13 +7,25 @@
 
 # Main code
 
+if [ $# -eq 0 ]
+	then
+	directory=$PWD
+elif [ -d $1 ]
+	then
+	directory=$1
+	else
+	echo "sorry $1 is not a directory" 
+
+fi
+
+
 ls -1 $1*.avi > videofiles
 
-while videofile
+while read videofile
 do
 	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
-	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
+	namefile=${videofile: -3}
+	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc
 
 done < videofiles
 rm videofiles
